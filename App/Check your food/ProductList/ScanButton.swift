8c295ee8bda2c27d3cf84be38f62db2769aa2cd1
Pascal Sauer <email@cubexs.dev>
2025-04-21 20:13:46 +0200

    [App] Add a button to the bottom right corner

---
+++ App/Check your food/ProductList/ScanButton.swift
@@ -0,0 +1,35 @@
+//
+//  ScanButton.swift
+//  Check your food
+//
+//  Created by Pascal Sauer on 21.04.25.
+//
+
+import SwiftUI
+
+struct ScanButton: View {
+    var callback: () -> Void
+
+    var body: some View {
+        VStack {
+            Spacer()
+            HStack {
+                Spacer()
+                Button(action: {
+                    callback()
+                }) {
+                    Image(systemName: "qrcode.viewfinder")
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.blue)
+                        .clipShape(Circle())
+                        .shadow(radius: 4)
+                }
+                .accessibilityLabel("Scan Barcode")
+                .padding()
+                
+            }
+        }
+    }
+}
