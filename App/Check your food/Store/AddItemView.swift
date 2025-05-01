967d91051a7e8d6ea8ace4d6fa03805f6a8469be
Pascal Sauer <email@cubexs.dev>
2025-05-01 13:47:50 +0200

    [App] Move Add Item form to new component

---
+++ App/Check your food/Store/AddItemView.swift
@@ -0,0 +1,51 @@
+//
+//  AddItemView.swift
+//  Check your food
+//
+//  Created by Pascal Sauer on 01.05.25.
+//
+
+import SwiftData
+import SwiftUI
+
+struct AddItemView: View {
+    @Environment(\.modelContext) var modelContext
+    
+    @Binding var selectedDate: Date
+    @Binding var isShowingScanSheet: Bool
+    
+    let searchedProduct: ProductModel
+    
+    var languageCode = Locale.current.language.languageCode?.identifier
+    
+    var body: some View {
+        Form {
+            Section {
+                ProductImage(product: searchedProduct)
+                Text(languageCode == "de" ? searchedProduct.productNameDE : searchedProduct.productNameEN)
+                    .font(.caption)
+                    .frame(maxWidth: .infinity)
+                    .multilineTextAlignment(.center)
+            }
+            
+            Section {
+                DatePicker("Best-Before date", selection: $selectedDate, displayedComponents: [.date])
+            }
+        }
+        HStack {
+            Spacer()
+            Button("Cancel", role: .cancel) {
+                isShowingScanSheet.toggle()
+            }
+            Spacer()
+            Spacer()
+            Button("Save") {
+                let item = StoreItem(bestBefore: selectedDate, product: searchedProduct)
+                modelContext.insert(item)
+                isShowingScanSheet.toggle()
+            }
+            Spacer()
+        }
+        .padding(32)
+    }
+}
